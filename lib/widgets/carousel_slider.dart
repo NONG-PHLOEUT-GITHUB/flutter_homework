52d13ddbdd332788e8b9ea4b527d00d11997d3ee
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2025-05-26 15:03:28 +0700

    first commit

--- lib/widgets/carousel_slider.dart
+++ lib/widgets/carousel_slider.dart
@@ -2,85 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
   @override
-  _ImageSliderState createState() => _ImageSliderState();
+  State<ImageSlider> createState() => _ImageSliderState();
 }
 
 class _ImageSliderState extends State<ImageSlider> {
   int _currentIndex = 0;
 
-  final List<String> imageUrls = [
-    "https://www.spized.com/media/75/f7/f8/1680187076/Fu%C3%9Fballfeld.jpg",
-    "https://t3.ftcdn.net/jpg/09/28/78/84/360_F_928788438_TyuKTkDqrR3GolJsgbltMcAgOufUAFhu.jpg",
-    "https://img.freepik.com/premium-photo/green-grass-soccer-stadium_30824-117.jpg",
-    "https://img.freepik.com/premium-photo/green-grass-soccer-stadium_30824-117.jpg",
+  final List<String> _imagePaths = [
+    'assets/images/ads1.webp',
+    'assets/images/ads2.png',
+    'assets/images/ads3.png',
+    'assets/images/ads4.webp',
   ];
+
+  final double _sliderHeight = 150.0;
+  final double _dotSize = 8.0;
+  final double _activeDotWidth = 20.0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // Image Slider
-        Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 6.0,
-                spreadRadius: 1.0,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: CarouselSlider(
-              items: imageUrls.map((url) => _buildImage(url)).toList(),
-              options: CarouselOptions(
-                height: 150.0,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                viewportFraction: 1.0,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-            ),
-          ),
-        ),
-
+        _buildCarousel(),
         const SizedBox(height: 8),
-
-        // Dot Indicator with active width effect
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: imageUrls.asMap().entries.map((entry) {
-            bool isActive = _currentIndex == entry.key;
-            return AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              height: 8.0,
-              width: isActive ? 20.0 : 8.0,
-              decoration: BoxDecoration(
-                color: isActive ? Colors.green : Colors.grey.shade400,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            );
-          }).toList(),
-        )
+        _buildDotIndicators(),
       ],
     );
   }
 
-  Widget _buildImage(String url) {
+  Widget _buildCarousel() {
     return Container(
       decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 6.0,
+            spreadRadius: 1.0,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: CarouselSlider(
+          items: _imagePaths.map((path) => _buildImage(path)).toList(),
+          options: CarouselOptions(
+            height: _sliderHeight,
+            autoPlay: true,
+            aspectRatio: 16 / 9,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            viewportFraction: 1.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDotIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _imagePaths.asMap().entries.map((entry) {
+        bool isActive = _currentIndex == entry.key;
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.symmetric(horizontal: 4.0),
+          height: _dotSize,
+          width: isActive ? _activeDotWidth : _dotSize,
+          decoration: BoxDecoration(
+            color: isActive ? Colors.green : Colors.grey.shade400,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildImage(String assetPath) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
         image: DecorationImage(
-          image: NetworkImage(url),
+          image: AssetImage(assetPath),
           fit: BoxFit.cover,
         ),
       ),
